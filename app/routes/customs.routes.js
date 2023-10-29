6574f02f45e5f8fe471b8b9d16e54a4f981bf841
Ahmed atta <ahmed.atta@asyadexpress.com>
2023-10-29 12:32:20 +0400

    customs routes

--- app/routes/customs.routes.js
+++ app/routes/customs.routes.js
@@ -4,7 +4,11 @@ module.exports = function (app) {
   app.get("/api/customs/house-manifest", [authJwt.verifyToken], controller.getHouseManifest);
   app.get("/api/customs/client-manifest", [authJwt.verifyToken], controller.getClientManifest);
   app.get("/api/customs/air-manifest", [authJwt.verifyToken], controller.getAirManifest);
-  app.post("/api/customs/house-manifest", [authJwt.verifyToken], controller.addHouseManifest);
+  app.get("/api/customs/declaration", [authJwt.verifyToken], controller.getDeclaration);
+  app.delete("/api/customs/air-manifest/:id", [authJwt.verifyToken], controller.deleteAirManifest);
   app.delete("/api/customs/house-manifest/:id", [authJwt.verifyToken], controller.deleteHouseManifest);
+  app.delete("/api/customs/declaration/:id", [authJwt.verifyToken], controller.deleteDeclaration);
+  app.post("/api/customs/house-manifest", [authJwt.verifyToken], controller.addHouseManifest);
   app.post("/api/customs/air-manifest", [authJwt.verifyToken], controller.addAirManifest);
+  app.post("/api/customs/declaration", [authJwt.verifyToken], controller.addDeclaration);
 };
