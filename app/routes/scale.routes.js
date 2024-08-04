05afeae5180f5af8827f3629efea710159d0f740
moh_burhan <m.hamed@fetchr.us>
2024-08-04 21:42:49 +0400

    scale data

--- app/routes/scale.routes.js
+++ app/routes/scale.routes.js
@@ -1,7 +1,7 @@
 const controller = require("../controllers/scale.controller");
 module.exports = function (app) {
-    app.get(
-        "/api/scale",
-        controller.pushSaleDataToMw
+    app.post(
+        "/api/scale-list",
+        controller.scaleDataList
     );
 }
