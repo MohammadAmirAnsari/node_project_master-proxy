b2d0bafe113c88082a7c68f68d84557c218c6622
Saeed Saleh <saeed.salih@omanpost.om>
2024-09-09 10:12:23 +0400

    fix url conflict

--- app/routes/pickup.routes.js
+++ app/routes/pickup.routes.js
@@ -2,12 +2,12 @@ const { authJwt } = require("../middleware");
 const controller = require("../controllers/pickup.controller");
 
 module.exports = function (app) {
-  app.get("/api/fetchAllPickup", [authJwt.verifyToken], controller.fetchAllPickup);
-  app.get("/api/fetchPickupByCountryMerchant", [authJwt.verifyToken], controller.fetchPickupByCountryMerchant);
-  app.get("/api/fetchMerchants", [authJwt.verifyToken], controller.fetchMerchants);
-  app.post("/api/createOrders", [authJwt.verifyToken], controller.createOrders);
-  app.put("/api/updateOrders/:id", [authJwt.verifyToken], controller.updateOrders);
-  app.get("/api/listOrders", [authJwt.verifyToken], controller.listOrders);
-  app.get("/api/listAddresses", [authJwt.verifyToken], controller.listAddresses);
-  app.post("/api/bulkUpdateOrders", controller.bulkUpdateOrders);
+  app.get("/api/pickup/fetchAll", [authJwt.verifyToken], controller.fetchAllPickup);
+  app.get("/api/pickup/fetchByCountryMerchant", [authJwt.verifyToken], controller.fetchPickupByCountryMerchant);
+  app.get("/api/pickup/fetchMerchants", [authJwt.verifyToken], controller.fetchMerchants);
+  app.post("/api/pickup/createOrders", [authJwt.verifyToken], controller.createOrders);
+  app.put("/api/pickup/updateOrders/:id", [authJwt.verifyToken], controller.updateOrders);
+  app.get("/api/pickup/listOrders", [authJwt.verifyToken], controller.listOrders);
+  app.get("/api/pickup/listAddresses", [authJwt.verifyToken], controller.listAddresses);
+  app.post("/api/pickup/bulkUpdateOrders", controller.bulkUpdateOrders);
 };
