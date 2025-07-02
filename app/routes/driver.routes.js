5182c1026036b66debc626cf3d0f9b4551127fa3
saeed.salih <saeed.salih@asyadexpress.com>
2025-07-02 10:03:48 +0400

    update links

--- app/routes/driver.routes.js
+++ app/routes/driver.routes.js
@@ -6,10 +6,11 @@ module.exports = function (app) {
   app.get("/api/banks", [authJwt.verifyToken], controller.fetchAllBanks);
   app.post("/api/driver", [authJwt.verifyToken], controller.createDriver);
   app.get("/api/fetchdriver/:id", [authJwt.verifyToken], controller.fetchDriver);
-  app.get("/api/reports", [authJwt.verifyToken], controller.fetchAllReports);
-  app.post("/api/reports", [authJwt.verifyToken], controller.createPayment);
   app.get("/api/reports/sendfinance/:id", [authJwt.verifyToken], controller.sendFinance);
   app.get("/api/reports/sendfinish/:id", [authJwt.verifyToken], controller.sendFinish);
+  app.get("/api/reports", [authJwt.verifyToken], controller.fetchAllReports);
+  app.post("/api/reports", [authJwt.verifyToken], controller.createPayment);
+
   app.post("/api/reports/:id", [authJwt.verifyToken], controller.updatePayment);
   app.get("/api/fetchreport/:id", [authJwt.verifyToken], controller.fetchReport);
 
