b77d36aab1aed820531a97b1138dc5f8dcdc88ce
saeed.salih <saeed.salih@asyadexpress.com>
2025-07-10 14:07:34 +0400

    fix data type

--- app/controllers/driver.controller.js
+++ app/controllers/driver.controller.js
@@ -42,6 +42,7 @@ exports.createDriver = async (req, res) => {
           res.status(200).json(response.data);
         })
         .catch((error) => {
+            console.log(response);
           res.status(500).json(error);
         });
   } catch (error) {
